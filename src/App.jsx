8fe2f4316be51e1e6b3e388aabb0624018265f0f
HaggardFelicia <HaggardFelicia@outlook.com>
2025-09-14 23:59:56 -0500

    working on responsive design.

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,10 @@ function App() {
           <h3>Perfume</h3>
           <h1>Gabrielle Essence Eau De Parfum</h1>
           <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
-          <h2>$149.99</h2>
-          <p className='price-markdown'>$169.99</p>
+          <div className='price-container'>
+            <h2>$149.99</h2>
+            <p className='price-markdown'>$169.99</p>
+          </div>
           <button><span><image src="./assets/icon-cart.svg" alt="shopping cart" /></span>Add to Cart</button>
         </div>
       </div>
